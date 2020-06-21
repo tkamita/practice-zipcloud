0e9fdbdfff1b272edff68b9807caeb1ebe41f4ab
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-06-22 00:15:13 +0900

    add geocoder

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'maps#index'
   resources :maps, only: [:index]
   get 'search', to: "search#search"
+  get '/map_request', to: 'maps#map', as: 'map_request'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
